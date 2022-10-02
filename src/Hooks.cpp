01c1394ae0ecf847fc5ce81d013a66642bac3eb0
powerof3 <32599957+powerof3@users.noreply.github.com>
2022-10-02 15:36:28 +0530

    refactor merge SE/AE

---
+++ src/Hooks.cpp
@@ -0,0 +1,80 @@
+#include "Hooks.h"
+
+namespace Hooks
+{
+	namespace NotAChild
+	{
+		void Install()
+		{
+			//FF 90 F0 02 00 00	- call qword ptr [rax+2F0h]
+
+			std::array targets{
+#ifdef SKYRIM_AE
+				std::make_pair(37229, 0x32),   // canKillMe.
+				std::make_pair(37896, 0x8F),   // killImpl.
+				std::make_pair(37690, 0xD1),   // applySkinBloodDecals.
+				std::make_pair(44217, 0x165),  // triggerMines.
+				std::make_pair(38627, 0x22F),  // applyBash.
+				std::make_pair(44031, 0x1E8),  // recoverArrows.
+				std::make_pair(54660, 0x97),   // damageActorValue_Script
+#else
+				std::make_pair(36247, 0x32),   // canKillMe
+				std::make_pair(36872, 0x87),   // killImpl
+				std::make_pair(36682, 0xE0),   // applySkinBloodDecals
+				std::make_pair(43026, 0x164),  // triggerMines
+				std::make_pair(37673, 0x220),  // applyBash
+				std::make_pair(42856, 0x1D1),  // recoverArrows
+				std::make_pair(53860, 0x97),   // damageActorValue_Script
+#endif
+			};
+
+			struct Patch : Xbyak::CodeGenerator
+			{
+				Patch()
+				{
+					call(qword[rax + 0x830]);  //unused vfunc? that returns false for actor classes
+				}
+			};
+
+			Patch patch;
+			patch.ready();
+
+			for (const auto& [id, offset] : targets) {
+				REL::Relocation<std::uintptr_t> target{ REL::ID(id) };
+				REL::safe_write(target.address() + offset, std::span{ patch.getCode(), patch.getSize() });
+			}
+		}
+	}
+
+	namespace MakeVunerable
+	{
+		struct IsInvunerable
+		{
+			static bool thunk(RE::MagicTarget* a_this)
+			{
+				auto result = func(a_this);
+				if (result && a_this) {
+					if (const auto actor = stl::adjust_pointer<RE::Character>(a_this, -0x98); actor && actor->IsChild()) {
+						result = false;
+					}
+				}
+				return result;
+			}
+			static inline REL::Relocation<decltype(thunk)> func;
+
+			static inline constexpr std::size_t size = 0x04;
+		};
+
+		void Install()
+		{
+			stl::write_vfunc<RE::Character, 4, IsInvunerable>();
+		    logger::info("Installed invunerability patch"sv);
+		}
+	}
+
+	void Install()
+	{
+		NotAChild::Install();
+		MakeVunerable::Install();
+	}
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,84 +1,26 @@
-namespace NotAChild
+#include "Hooks.h"
+
+void MessageHandler(SKSE::MessagingInterface::Message* a_message)
 {
-	void Install()
-	{
-		//FF 90 F0 02 00 00	- call qword ptr [rax+2F0h]
-
-		std::array targets{
-			std::make_pair(36247, 0x32),   // canKillMe
-			std::make_pair(36872, 0x87),   // killImpl
-			std::make_pair(36682, 0xE0),   // applySkinBloodDecals
-			std::make_pair(43026, 0x164),  // triggerMines
-			std::make_pair(37673, 0x220),  // applyBash
-			std::make_pair(42856, 0x1D1),  // recoverArrows
-			std::make_pair(53860, 0x97),   // damageActorValue_Script
-		};
-
-		struct Patch : Xbyak::CodeGenerator
-		{
-			Patch()
-			{
-				call(qword[rax + 0x830]);  //unused vfunc? that returns false for actor classes
-			}
-		};
-
-		Patch patch;
-		patch.ready();
-
-		for (const auto& [id, offset] : targets) {
-			REL::Relocation<std::uintptr_t> target{ REL::ID(id) };
-			REL::safe_write(target.address() + offset, std::span{ patch.getCode(), patch.getSize() });
-		}
+	if (a_message->type == SKSE::MessagingInterface::kPostLoad) {
+		Hooks::Install();
 	}
 }
 
-namespace MakeVunerable
-{
-	struct IsInvunerable
-	{
-		static bool thunk(RE::MagicTarget* a_this)
-		{
-			auto result = func(a_this);
-			if (result && a_this) {
-				if (const auto actor = stl::adjust_pointer<RE::Character>(a_this, -0x98); actor && actor->IsChild()) {
-					result = false;
-				}
-			}
-			return result;
-		}
-		static inline REL::Relocation<decltype(thunk)> func;
+#ifdef SKYRIM_AE
+extern "C" DLLEXPORT constinit auto SKSEPlugin_Version = []() {
+	SKSE::PluginVersionData v;
+	v.PluginVersion(Version::MAJOR);
+	v.PluginName("Slayable Offspring SKSE");
+	v.AuthorName("powerofthree");
+	v.UsesAddressLibrary(true);
+	v.CompatibleVersions({ SKSE::RUNTIME_LATEST });
 
-		static inline constexpr std::size_t size = 0x04;
-	};
-
-	inline void Install()
-	{
-		stl::write_vfunc<RE::Character, 4, IsInvunerable>();
-		logger::info("Installed invunerability patch"sv);
-	}
-}
-
+	return v;
+}();
+#else
 extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Query(const SKSE::QueryInterface* a_skse, SKSE::PluginInfo* a_info)
 {
-	auto path = logger::log_directory();
-	if (!path) {
-		return false;
-	}
-
-	*path /= Version::PROJECT;
-	*path += ".log"sv;
-	auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true);
-
-	auto log = std::make_shared<spdlog::logger>("global log"s, std::move(sink));
-
-	log->set_level(spdlog::level::info);
-	log->flush_on(spdlog::level::info);
-
-	spdlog::set_default_logger(std::move(log));
-	spdlog::set_pattern("[%H:%M:%S] %v"s);
-
-	logger::info(FMT_STRING("{} v{}"), Version::PROJECT, Version::NAME);
-
 	a_info->infoVersion = SKSE::PluginInfo::kVersion;
 	a_info->name = "Slayable Offspring SKSE";
 	a_info->version = Version::MAJOR;
@@ -96,15 +38,39 @@ extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Query(const SKSE::QueryInterface* a
 
 	return true;
 }
+#endif
+
+void InitializeLog()
+{
+	auto path = logger::log_directory();
+	if (!path) {
+		stl::report_and_fail("Failed to find standard logging directory"sv);
+	}
+
+	*path /= fmt::format(FMT_STRING("{}.log"), Version::PROJECT);
+	auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true);
+
+	auto log = std::make_shared<spdlog::logger>("global log"s, std::move(sink));
+
+	log->set_level(spdlog::level::info);
+	log->flush_on(spdlog::level::info);
+
+	spdlog::set_default_logger(std::move(log));
+	spdlog::set_pattern("[%l] %v"s);
+
+	logger::info(FMT_STRING("{} v{}"), Version::PROJECT, Version::NAME);
+}
 
 extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* a_skse)
 {
-	logger::info("loaded");
+	InitializeLog();
+
+	logger::info("Game version : {}", a_skse->RuntimeVersion().string());
 
 	SKSE::Init(a_skse);
 
-	NotAChild::Install();
-	MakeVunerable::Install();
+	const auto messaging = SKSE::GetMessagingInterface();
+	messaging->RegisterListener(MessageHandler);
 
 	return true;
 }
